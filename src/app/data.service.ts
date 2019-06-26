8e2efca99c11f57c70de33694f067558c2054090
joaquin lema <joaco.lema@hotmail.com>
2019-06-25 21:26:42 -0300

    asd

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Persona } from './persona.model';
@@ -34,33 +33,3 @@ export class DataService {
     );
   }
 }
-=======
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Persona } from './persona.model';
-
-@Injectable()
-export class DataService {
-  constructor(private httpClient: HttpClient) {}
-
-  onGuardarPersonas( personas: Persona[]) {
-    this.httpClient.put('https://app-listado-personas-64fad.firebaseio.com/datos.json', personas).subscribe(
-      response => { console.log('resultado de guardar personas' + response); },
-      error => { console.log('error' + error); }
-    );
-  }
-
-  onGetPersonas() {
-    return this.httpClient.get('https://app-listado-personas-64fad.firebaseio.com/datos.json');
-  }
-
-  onModificarPersona(indice: number, persona: Persona) {
-    const url: string = 'https://app-listado-personas-64fad.firebaseio.com/datos/' + indice + '.json';
-
-    this.httpClient.put(url, persona).subscribe(
-      response => { console.log('resultado de modificar personas' + response); },
-      error => { console.log('error' + error); }
-    );
-  }
-}
->>>>>>> 640188f062269582063479223714d841a0c7db50
