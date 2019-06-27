487d8841515835d41d4be66d66f6faddfd0fe9ac
joaquin lema <joaco.lema@hotmail.com>
2019-06-27 19:05:51 -0300

    reorganizamos componentes agregando personasComponent en imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from '../app.component';
-import { PersonaComponent } from './persona/persona.component';
-import { FormularioComponent } from './formulario/formulario.component';
-import { LoggingService } from '../LoggingService.service';
-import { PersonasService } from '../personas.service';
-import { FormularioAngularMaterialComponent } from './formulario-angular-material/formulario-angular-material.component';
+import { AppComponent } from './app.component';
+import { PersonaComponent } from './personas/persona/persona.component';
+import { FormularioComponent } from './personas/formulario/formulario.component';
+import { LoggingService } from './LoggingService.service';
+import { PersonasService } from './personas.service';
+import { FormularioAngularMaterialComponent } from './personas/formulario-angular-material/formulario-angular-material.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule,
         MatButtonModule, MatSidenavModule,
         MatIconModule, MatListModule,
         MatFormFieldModule, MatInputModule,
         MatSelectModule, MatOptionModule } from '@angular/material';
-import { DataService } from '../data.service';
+import { DataService } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
-import { FormularioEdicionComponent } from './formulario-edicion/formulario-edicion.component';
+import { FormularioEdicionComponent } from './personas/formulario-edicion/formulario-edicion.component';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { LoginComponent } from '../login/login.component';
-import { AppRoutingModule } from '../app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { AppRoutingModule } from '../app-routing.module';
     FormularioComponent,
     FormularioAngularMaterialComponent,
     FormularioEdicionComponent,
-    LoginComponent
+    LoginComponent,
+    PersonasComponent
   ],
   imports: [
     BrowserModule,
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from '../persona.model';
+import { PersonasService } from '../personas.service';
 
 @Component({
   selector: 'app-personas',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonasComponent implements OnInit {
 
-  constructor() { }
+  title = 'listado-personas';
+  _personas: Persona[];
+  mostrarEdicion: boolean;
+  persona: Persona;
+  indice: number;
 
-  ngOnInit() {
+  constructor(private personasService: PersonasService) {}
+
+  ngOnInit(): void {
+    this.persona = new Persona('', '');
+    // this._personas = this.personasService.onObtenerPersonas();
+    this.personasService.onObtenerPersonas().subscribe(
+      (personas: Persona[]) => {
+        this._personas = personas;
+        this.personasService.setPersonas(this._personas);
+        this.mostrarEdicion = false;
+      }
+    );
+  }
+
+  onLlamarEdicion(obj: any) {
+    this.mostrarEdicion = true;
+    const personaSeleccionada = new Persona(obj.persona._nombre, obj.persona._apellido);
+    this.persona = personaSeleccionada;
+    this.indice = obj.indice;
+  }
+
+  cerrarForm(cerrarFormulario: boolean) {
+    this.mostrarEdicion = cerrarFormulario;
   }
 
 }
