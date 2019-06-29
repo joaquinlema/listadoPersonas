070a2618a0fc7c7fd22a374cac182d7209d87b21
joaquin lema <joaco.lema@hotmail.com>
2019-06-29 18:23:07 -0300

    agregamos rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { PersonasComponent } from './personas/personas.component';
+import { FormularioAngularMaterialComponent } from './personas/formulario-angular-material/formulario-angular-material.component';
 
+const routes: Routes = [
+  {path : '', component: PersonasComponent},
+  {path : 'personas', component: PersonasComponent},
+  {path : 'personas/agregar', component: FormularioAngularMaterialComponent},
+  {path : 'personas/:id', component: FormularioAngularMaterialComponent},
+]
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(
+      routes
+    )
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
