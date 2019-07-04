b80898aed53a2dcbef34220154dfcf57b7e4ae92
joaquin lema <joaco.lema@hotmail.com>
2019-07-04 15:53:59 -0300

    agregamos funcionalidad ed botones login y logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { LoginService } from './login/login.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  constructor(private loginService: LoginService) {}
 
   ngOnInit(): void {
       firebase.initializeApp({
@@ -14,5 +17,12 @@ export class AppComponent implements OnInit {
       });
   }
 
+  isAutenticado() {
+
+  }
+
+  onSalir() {
+
+  }
 
 }
