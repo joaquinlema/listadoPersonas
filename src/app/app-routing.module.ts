c0320b10826db78b4fac27ddf35ef42dcfe9a912
joaquin lema <joaco.lema@hotmail.com>
2019-07-04 09:53:38 -0300

    agregamos loggin a routing del modulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioAngularMaterialComponent } from './personas/formulario-angular-material/formulario-angular-material.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path : '', component: PersonasComponent},
-  {path : 'personas', component: PersonasComponent},
-  {path : 'personas/agregar', component: FormularioAngularMaterialComponent},
-  {path : 'personas/:id', component: FormularioAngularMaterialComponent},
+  {path : 'personas', component: PersonasComponent, children : [
+    {path : 'personas/agregar', component: FormularioAngularMaterialComponent},
+    {path : 'personas/:id', component: FormularioAngularMaterialComponent},
+  ]},
+  {path: 'login', component: LoginComponent}
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(
