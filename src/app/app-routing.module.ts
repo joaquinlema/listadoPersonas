f6834d0ebec3c379a867612aaf9fd0524a59ddf8
joaquin lema <joaco.lema@hotmail.com>
2019-07-04 17:11:17 -0300

    agregamos guardianes a cada url para redireccionar en caso que no este loggeado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { PersonasComponent } from './personas/personas.component';
 import { FormularioAngularMaterialComponent } from './personas/formulario-angular-material/formulario-angular-material.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { LoginGuardian } from './login/login-guardian.service';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path : '', component: PersonasComponent},
-  {path : 'personas', component: PersonasComponent, children : [
+  {path : '', canActivate: [LoginGuardian], component: PersonasComponent},
+  {path : 'personas', component: PersonasComponent, canActivate: [LoginGuardian] , children : [
     {path : 'agregar', component: FormularioAngularMaterialComponent},
     {path : ':id', component: FormularioAngularMaterialComponent},
   ]},
--- src/app/login/login-guardian.service.ts
+++ src/app/login/login-guardian.service.ts
@@ -1,6 +1,8 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { LoginService } from './login.service';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class LoginGuardian implements CanActivate {
 
   constructor(private loginService: LoginService,
